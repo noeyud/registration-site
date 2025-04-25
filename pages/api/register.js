d9e69fe5e44dbb48e7688d32adc107bb36b3f04a
김두연 <duyeon7737@gmail.com>
2025-04-25 13:39:48 +0900

    feat: derivePassword 적용 및 API 수정

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,18 +1,26 @@
-import crypto from 'crypto';
+// pages/api/register.js
+import { derivePassword } from '../../lib/password';
 
-export default function handler(req, res) {
+
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-  const { sn, storeName, storeLocation, phone, installDate, businessRegNo, purchaseSource } = req.body;
+
+  const {
+    sn,
+    storeName,
+    storeLocation,
+    phone,
+    installDate,
+    businessRegNo,
+    purchaseSource
+  } = req.body;
 
   // TODO: 실제로는 DB에 저장하는 로직을 여기에 추가하세요.
 
-  // HMAC-SHA256으로 6자리 OTP 생성
-  const secret = process.env.OTP_SECRET;
-  const hmac = crypto.createHmac('sha256', secret).update(sn).digest('hex');
-  const num = parseInt(hmac, 16) % 1000000;
-  const otp = num.toString().padStart(6, '0');
+ // derivePassword()로 고정된 6자리 비밀번호 생성
+  const otp = derivePassword(sn);
 
-  res.status(200).json({ otp });
+  return res.status(200).json({ otp });
 }
